c88a63685479a2f1a19fd640eac4140ae235f0f9
albarracin-adn <84801884+albarracin-adn@users.noreply.github.com>
2023-06-08 01:26:47 +0200

    Material_Entregable_JS

--- Material_Entregable1/js/maps.js
+++ Material_Entregable1/js/maps.js
@@ -1,5 +1,9 @@
 const metros = document.querySelector("#station")
 const coordinates = document.querySelector("#coordinates")
+const bicycle = document.querySelector("#bicycle-station")
+const bicycleCoord = document.querySelector("#coordinates-bicycle")
+const nameBus = document.querySelector("#bus-station")
+const coordinatesBus = document.querySelector("#location-bus")
 
 //Coger los datos de la Api de lo que se quiere y guardarlos en una constante
 const myquery = `
@@ -19,7 +23,41 @@ query metros ($cursor:String) {
         endCursor
       }
     }
-    
+    metroStation (
+      findBy: {closest: {latitude:41.4085865, longitude: 2.1274931 } }
+    )
+    {
+      ...on MetroStation{
+        name
+        lines
+        coordinates {
+          latitude
+          longitude
+        }
+      }
+    }
+    bikeStation(
+      findBy: { closest: { latitude: 41.4085865, longitude: 2.1274931 } }
+    ) {
+      ...on BikeStation {
+        name
+        coordinates {
+          longitude
+          latitude
+        }
+        
+      }
+    } 
+    busStop (
+      findBy: {closest: {latitude: 41.4085865, longitude: 2.1274931 } }
+      ) {
+      ...on BusStop {
+        name
+        location {
+          address
+      }
+    }
+  }
   
   }
   
@@ -43,34 +81,99 @@ const response = await fetch(url, {
     
   .then(data => {
     // Aquí puedes acceder a los datos del metro más cercano
-    let MetroNombre = data.data.metroStations;
-    let name = MetroNombre.edges[0].node.name;
-    let latitude = MetroNombre.edges[0].node.coordinates.latitude;
-    let longitude = MetroNombre.edges[0].node.coordinates.longitude;
+    //let MetroNombre = data.data.metroStations;
+    //let name = MetroNombre.edges[0].node.name;
+    //let latitude = MetroNombre.edges[0].node.coordinates.latitude;
+    //let longitude = MetroNombre.edges[0].node.coordinates.longitude;
 
+    let stationmetro = data.data.metroStation
+    let nombrestation = stationmetro.name
+    let linestation = stationmetro.lines
+    let latitudemetro = stationmetro.coordinates.latitude
+    let longitudemetro = stationmetro.coordinates.longitude
+    console.log(stationmetro)
+    console.log(nombrestation)
+    console.log(linestation)
+    console.log(latitudemetro)
+    console.log(longitudemetro)
+    //acceder a los datos de la bicicletas
+    let bici = data.data.bikeStation;
+    let namebici = bici.name;
+    let bicylongitud = bici.coordinates.longitude
+    let bicylatitud = bici.coordinates.latitude
+    //console.log(bici)
+    //console.log(namebici)
+    //console.log("Longitud: " + bicylongitud)
+    //console.log("Latitud: " + bicylatitud)
+
+    //acceder a los datos de los buses
+    let bus = data.data.busStop
+    let busName = bus.name
+    let busAddress = bus.location.address
+    //console.log(bus)
+    //console.log(busName)
+    //console.log(busAddress)
     // Haz lo que desees con los datos del metro
 
     // crear constante para visualización del nombre de metro
+    /*
     const article = document.createElement("article")
     article.innerText = name
     metros.appendChild(article)
     article.classList.add("px-3")
+    */
 
-    // crear constante para visualización de latitude y longitude
+    const nameMetro = document.createElement("article")
+    nameMetro.innerText = nombrestation + ", " + linestation
+    metros.appendChild(nameMetro)
+    nameMetro.classList.add("px-3")
+
+    // crear constante para visualización de latitude y longitude del metro
+   /*
     const coord = document.createElement("article")
     coord.innerText = latitude + ", " + longitude;
     coordinates.appendChild(coord)
     coord.classList.add("px-3")
+    */
 
+    const coordinatesMetro = document.createElement("article")
+    coordinatesMetro.innerText = latitudemetro + ", " + longitudemetro
+    coordinates.appendChild(coordinatesMetro)
+    coordinatesMetro.classList.add("px-3")
+    // crear constante para visualización de nombre de bicicletas
+    const bicyStations = document.createElement("article")
+    bicyStations.innerText = namebici
+    bicycle.appendChild(bicyStations)
+    bicyStations.classList.add("px-3")
 
-    console.log(MetroNombre)
-    console.log("Nombre del metro más cercano: " + name);
-    console.log("Latitud: " + latitude);
-    console.log("Longitud: " + longitude);
+    // crear constante para visualización de coordenadas de bicicletas
+    const coordinatesBicy = document.createElement("article")
+    coordinatesBicy.innerText = bicylatitud + ", " + bicylongitud;
+    bicycleCoord.appendChild(coordinatesBicy)
+    coordinatesBicy.classList.add("px-3")
+
+    // crear constante para visualización de buses
+
+    const busStation = document.createElement("article")
+    busStation.innerText = busName
+    nameBus.appendChild(busStation)
+    busStation.classList.add("px-3")
+
+    // crear constante para visualizacion de direccion de bus
+
+    const busCoordinates = document.createElement("article")
+    busCoordinates.innerText = busAddress
+    coordinatesBus.appendChild(busCoordinates)
+    busCoordinates.classList.add("px-3")
+
+    //console.log(MetroNombre)
+    //console.log("Nombre del metro más cercano: " + name);
+    //console.log("Latitud: " + latitude);
+    //console.log("Longitud: " + longitude);
   })
 .catch(error => console.error(error))
 //console.log(text)
-console.log(response)
+//console.log(response)
 
 
 
