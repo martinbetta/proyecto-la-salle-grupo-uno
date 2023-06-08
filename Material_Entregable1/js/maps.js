2bf83118bd11f29cf1564210b8691dafef7c9984
albarracin-adn <84801884+albarracin-adn@users.noreply.github.com>
2023-06-08 19:00:53 +0200

    Material_Entregable_JS

--- Material_Entregable1/js/maps.js
+++ Material_Entregable1/js/maps.js
@@ -1,3 +1,4 @@
+// definir constantes que se usarán para visualizar el contenido
 const metros = document.querySelector("#station")
 const coordinates = document.querySelector("#coordinates")
 const bicycle = document.querySelector("#bicycle-station")
@@ -5,25 +6,30 @@ const bicycleCoord = document.querySelector("#coordinates-bicycle")
 const nameBus = document.querySelector("#bus-station")
 const coordinatesBus = document.querySelector("#location-bus")
 
+if (navigator.geolocation) {
+  // El navegador soporta la geolocalización
+  navigator.geolocation.getCurrentPosition(function(position) {
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
+    
+    // Aquí puedes hacer lo que desees con las coordenadas
+    console.log("Latitud: " + latitude + " Longitud: " + longitude);
+    
+  }, function(error) {
+    // Error al obtener la localización
+    console.log("Error al obtener la localización: " + error.message);
+  });
+} else {
+  // El navegador no soporta la geolocalización
+  console.log("Geolocalización no soportada por el navegador");
+}
+
+async function getClosestStation(){
 //Coger los datos de la Api de lo que se quiere y guardarlos en una constante
 const myquery = `
-query metros ($cursor:String) {
-    metroStations(first:2 after:$cursor ){
-      edges {
-        node {
-          name
-          coordinates {
-            latitude
-            longitude
-          }
-        }
-      }
-      pageInfo {
-        hasNextPage
-        endCursor
-      }
-    }
-    metroStation (
+query stations {
+    
+    closestMetroStation: metroStation (
       findBy: {closest: {latitude:41.4085865, longitude: 2.1274931 } }
     )
     {
@@ -36,7 +42,7 @@ query metros ($cursor:String) {
         }
       }
     }
-    bikeStation(
+    closestBikeStation: bikeStation(
       findBy: { closest: { latitude: 41.4085865, longitude: 2.1274931 } }
     ) {
       ...on BikeStation {
@@ -48,7 +54,7 @@ query metros ($cursor:String) {
         
       }
     } 
-    busStop (
+    closestBusStation: busStop (
       findBy: {closest: {latitude: 41.4085865, longitude: 2.1274931 } }
       ) {
       ...on BusStop {
@@ -64,40 +70,45 @@ query metros ($cursor:String) {
 `
 //indicar la url de la API (de Saverio)
 const url = "https://healthy-fox-82.deno.dev/graphql"
-const variables = { cursor: null }
+//const variables = { cursor: null }
 //esperar a la url, con el metodo post, y el body a mostrar.
 const response = await fetch(url, {
   method: "POST",
-  body: JSON.stringify({ query: myquery, variables: variables })
+  body: JSON.stringify({ query: myquery})
 })
-  //const text = await response.text() original del profe
-  .then(response => {
+//const text = await response.text()// original del profe
+const data = await response.json()
+
+  console.log(data)
+  //.then(response => {
     // response.ok será true con respuestas 2XX
-    if (!response.ok){
-      throw new Error("Error HTTP: " + response.status)
-    }
-    return response.json()
-  })
+    //if (!response.ok){
+      //throw new Error("Error HTTP: " + response.status)
+   // }
+   // return response.json()
+  //})
     
-  .then(data => {
+  
     // Aquí puedes acceder a los datos del metro más cercano
     //let MetroNombre = data.data.metroStations;
     //let name = MetroNombre.edges[0].node.name;
     //let latitude = MetroNombre.edges[0].node.coordinates.latitude;
     //let longitude = MetroNombre.edges[0].node.coordinates.longitude;
 
-    let stationmetro = data.data.metroStation
+    let stationmetro = data.data.closestMetroStation
     let nombrestation = stationmetro.name
     let linestation = stationmetro.lines
     let latitudemetro = stationmetro.coordinates.latitude
     let longitudemetro = stationmetro.coordinates.longitude
-    console.log(stationmetro)
-    console.log(nombrestation)
-    console.log(linestation)
-    console.log(latitudemetro)
-    console.log(longitudemetro)
+    //console.log(stationmetro)
+    //console.log(nombrestation)
+    //console.log(linestation)
+    //console.log(latitudemetro)
+    //console.log(longitudemetro)
+
+
     //acceder a los datos de la bicicletas
-    let bici = data.data.bikeStation;
+    let bici = data.data.closestBikeStation;
     let namebici = bici.name;
     let bicylongitud = bici.coordinates.longitude
     let bicylatitud = bici.coordinates.latitude
@@ -107,7 +118,7 @@ const response = await fetch(url, {
     //console.log("Latitud: " + bicylatitud)
 
     //acceder a los datos de los buses
-    let bus = data.data.busStop
+    let bus = data.data.closestBusStation
     let busName = bus.name
     let busAddress = bus.location.address
     //console.log(bus)
@@ -170,11 +181,12 @@ const response = await fetch(url, {
     //console.log("Nombre del metro más cercano: " + name);
     //console.log("Latitud: " + latitude);
     //console.log("Longitud: " + longitude);
-  })
-.catch(error => console.error(error))
+  
+//.catch(error => console.error(error))
 //console.log(text)
 //console.log(response)
-
+}
+getClosestStation()
 
 
 
