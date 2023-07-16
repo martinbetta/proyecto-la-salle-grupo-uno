d068ebcc0a2cd45e5856b22047ae5b708f719d98
albarracin-adn <84801884+albarracin-adn@users.noreply.github.com>
2023-07-16 20:25:52 +0200

    EntregableVuejs

--- vue-project/src/components/NotFound.vue
+++ vue-project/src/components/NotFound.vue
@@ -1,7 +1,23 @@
 <template>
-  <div class="text-center">
-    <h1>Página no encontrada</h1>
-    <p>Oops! La página no existe. :P</p>
+  <div class="fondo row g-0">
+    <div class="m-auto col-auto text-center card ancho">
+      <img
+        src="../assets/image/Cuatrocerocuatro.jpeg"
+        class="card-img-top"
+        alt="404"
+      />
+      <h1 class="card-title">Página no encontrada</h1>
+      <p class="card-text">
+        Oops! La página no existe. <br />
+        Más suerte la próxima vez.
+      </p>
+
+      <router-link
+        class="text-white text-decoration-none btn btn-primary"
+        to="/"
+        >Ve al Inicio</router-link
+      >
+    </div>
   </div>
 </template>
 
@@ -13,4 +29,19 @@ export default {
 
 <style scoped>
 /* Estilos específicos para el componente NotFound.vue */
+.fondo {
+  background-image: url("../assets/image/Found.jpg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  position: fixed;
+  z-index: -1;
+  width: 100%;
+  height: 100%;
+}
+.ancho {
+  width: max-content;
+}
+.ancho img {
+  height: 40vh;
+}
 </style>
